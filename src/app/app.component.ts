c130be0238deab0ef56d0553c810b6624b059463
Khumozin <Khumomogorosi@gmail.com>
2023-06-08 16:07:00 +0200

    implement footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,31 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, HostBinding, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { map, of } from 'rxjs';
+
+import { environment } from '../environments/environment.development';
+import { Release } from './core/models';
+import { FooterComponent } from './shared';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'todo';
+
+  private http = inject(HttpClient);
+
+  public buildDate$ = environment.production
+    ? this.http
+        .get<Release>(`${environment.repoUrl}${environment.appVersion}`)
+        .pipe(map((release) => new Date(release.published_at)))
+    : of(new Date());
+
+  @HostBinding('attr.app-version')
+  public appVersion = `v${environment.appVersion}`;
 }
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  appVersion: require('../../package.json').version + '--dev',
+  repoUrl: 'https://api.github.com/repos/khumozin/todo/releases/tags/v',
+  repoToken: '',
+};

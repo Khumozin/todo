1b87b611b1581cfeff61b1a47c47b0f74664a57f
Khumozin <Khumomogorosi@gmail.com>
2023-06-08 17:22:57 +0200

    feat(features): implement todos feature

---
+++ src/app/features/todos/components/index.ts
@@ -0,0 +1,2 @@
+export * from './todos-form/todos-form.component';
+export * from './todos-list/todos-list.component';
---
+++ src/app/features/todos/components/todos-form/todos-form.component.ts
@@ -0,0 +1,38 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+import { FormType, FormValue } from '../../models';
+
+const UI = [MatFormFieldModule, MatInputModule, MatButtonModule];
+
+
+
+@Component({
+  selector: 'todos-form',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, ...UI],
+  templateUrl: './todos-form.component.html',
+  styleUrls: ['./todos-form.component.scss'],
+})
+export class TodosFormComponent {
+  @Output() onSave = new EventEmitter<Partial<FormValue>>();
+
+  public form = new FormGroup<FormType>({
+    Task: new FormControl(null, [Validators.required]),
+  });
+
+  onClick(): void {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+
+    const { value } = this.form;
+    this.onSave.emit(value);
+    this.form.reset();
+  }
+}

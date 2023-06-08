82d76380945a4f8250d571df1d2c88cd959d6ef8
Khumozin <Khumomogorosi@gmail.com>
2023-06-08 18:38:29 +0200

    feat(todos): add firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,21 @@
 import { provideHttpClient } from '@angular/common/http';
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
+import { environment } from 'src/environments/environment.development';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimations(), provideHttpClient()],
+  providers: [
+    provideRouter(routes),
+    provideAnimations(),
+    provideHttpClient(),
+    importProvidersFrom(
+      provideFirestore(() => getFirestore()),
+      provideFirebaseApp(() => initializeApp(environment.firebaseConfig))
+    ),
+  ],
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,12 @@
 export const environment = {
+  firebaseConfig: {
+    apiKey: 'AIzaSyA7hkNTSG880-M8HCEdCtFIc9EmAZekHwM',
+    authDomain: 'todo-ae31f.firebaseapp.com',
+    projectId: 'todo-ae31f',
+    storageBucket: 'todo-ae31f.appspot.com',
+    messagingSenderId: '1019369024071',
+    appId: '1:1019369024071:web:02de88bec90671a8745f62',
+  },
   production: true,
   appVersion: require('../../package.json').version,
   repoUrl: 'https://api.github.com/repos/khumozin/todo/releases/tags/v',
